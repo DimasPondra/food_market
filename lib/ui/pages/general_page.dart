8a97add12aa81ba979b2fec07ac0480607cdb06b
Dimas Pondra Oktafianto <77646429+DimasPondra@users.noreply.github.com>
2023-10-14 07:39:21 +0700

    :sparkles: add general page (app bar)

---
+++ lib/ui/pages/general_page.dart
@@ -0,0 +1,93 @@
+part of 'pages.dart';
+
+class GeneralPage extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final Function? onBackButtonPressed;
+  final Widget? child;
+  final Color? backColor;
+
+  const GeneralPage({
+    super.key,
+    this.title = 'Title',
+    this.subtitle = 'Subtitle',
+    this.onBackButtonPressed,
+    this.child,
+    this.backColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.white,
+          ),
+          SafeArea(
+              child: Container(
+            color: backColor ?? "FAFAFC".toColor(),
+          )),
+          SafeArea(
+            child: ListView(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: 100,
+                      color: Colors.white,
+                      margin: const EdgeInsets.only(bottom: defaultMargin),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: defaultPadding,
+                      ),
+                      child: Row(
+                        children: [
+                          onBackButtonPressed != null
+                              ? Container(
+                                  width: 24,
+                                  height: 24,
+                                  margin: const EdgeInsets.only(right: 26),
+                                  decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                      image:
+                                          AssetImage('assets/back_arrow.png'),
+                                    ),
+                                  ),
+                                )
+                              : const SizedBox(),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                title,
+                                style: GoogleFonts.poppins(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text(
+                                subtitle,
+                                style: GoogleFonts.poppins(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w300,
+                                  color: "8D92A3".toColor(),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    child ?? const SizedBox(),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
